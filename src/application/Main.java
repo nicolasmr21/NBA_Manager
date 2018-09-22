e2a682899079ec9f98f14a1d6483bfb7d4f9c421
Nicolas Martinez-Juan Manuel Castillo <nicolasmartinez502@gmail.com>
2018-09-22 10:20:25 -0500

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+	private Stage principal;
+
+	
+	@Override
+	public void start(Stage primaryStage) {
+		principal = primaryStage;
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/resources/Main.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			principal.setScene(scene);
+			principal.initStyle(StageStyle.UNDECORATED);
+			principal.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+	}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
